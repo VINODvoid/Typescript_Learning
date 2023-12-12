7bd35ed13c12f38081314d36bb28126e822aff29
VINODvoid <iamlearner.v@gmail.com>
2023-12-12 21:07:35 +0530

    intro

---
+++ 01intro/intro.js
@@ -0,0 +1,6 @@
+console.log("Hello World!");
+var user = {
+    name: "John",
+    age: 30
+};
+console.log(user.name);
---
+++ 01intro/intro.ts
@@ -0,0 +1,8 @@
+console.log("Hello World!");
+
+let user = {
+    name: "John",
+    age: 30
+};
+console.log(user.email);
+
