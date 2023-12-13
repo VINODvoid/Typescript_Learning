d808654246bf41787daf0749ee8dcb9e36b5319f
VINODvoid <iamlearner.v@gmail.com>
2023-12-13 05:57:52 +0530

    Add myFunction.js and myFunction.ts files

---
+++ 02basics/myFunction.js
@@ -0,0 +1,21 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+function addTwo(num) {
+    return num + 2;
+}
+function getUpper(val) {
+    return val.toUpperCase();
+}
+addTwo(2);{
+
+}
+getUpper("two");
+function signUpUser(name, email, password, isPaid) { }
+signUpUser("kalki", "email", "password", false);
+var loginUser = function (name, email, isPaid) {
+    if (isPaid === void 0) { isPaid = false; }
+};
+loginUser("kalki", "email");
+{
+
+}
---
+++ 02basics/myFunction.ts
@@ -0,0 +1,21 @@
+function addTwo(num: number){
+    return num + 2;
+}
+
+
+function getUpper(val:string)
+{
+    return val.toUpperCase();
+}
+
+addTwo(2)
+getUpper("two")
+
+function signUpUser (name:string, email:string, password:string,isPaid:boolean)
+{}
+
+signUpUser("kalki","email","password",false)
+
+let loginUser = (name:string,email:string, isPaid:boolean=false) => {}
+loginUser("kalki","email")
+export {}
