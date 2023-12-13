c9e4d04ad94401a703e687a2b3de1dd7406388bf
VINODvoid <iamlearner.v@gmail.com>
2023-12-13 23:31:13 +0530

    type keyboard

--- 02basics/myObjects.ts
+++ 02basics/myObjects.ts
@@ -4,16 +4,71 @@ const User= {
     country: 'India'
 }
 
-function createUser({name:string, isPaid:boolean})
-{
+// function createUser({name:string, isPaid:boolean})
+// {
 
+// }
+
+// createUser({name: 'Rajat', isPaid: true})
+
+// function createCourse ():{}
+// {
+//     return {}
+// }
+
+
+type User ={
+    name: string,
+    email:string,
+    isActive:boolean
 }
 
-createUser({name: 'Rajat', isPaid: true})
-
-function createCourse ():{}
+function CreateUser(user: User)
 {
-    return {}
+    return {name:"",email:"",isActive:false}
 }
+CreateUser({name: 'Rajat', email: '', isActive: false})
+
+
+type str = {
+    a: string;
+    b: string;
+}
+
+const create = (val:str) => {
+    console.log(val.a);
+    console.log(val.b);
+}
+
+
+
+
+type UserInputSanitizedString = string;
+ 
+function sanitizeInput(str: string): UserInputSanitizedString {
+  return sanitize(str);
+}
+ 
+// Create a sanitized input
+let userInput = sanitizeInput(getInput());
+ 
+// Can still be re-assigned with a string though
+userInput = "new input";
+
+
+
+
+
+
+
+
 
 export {}
+
+function sanitize(str: string): string {
+    throw new Error("Function not implemented.")
+}
+function getInput(): string {
+    throw new Error("Function not implemented.")
+}
+
