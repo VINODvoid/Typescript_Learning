bdd83791c0c495b0b3682f819f63db5590ac8239
VINODvoid <iamlearner.v@gmail.com>
2023-12-13 06:09:30 +0530

    added function -02 myFunction.ts

--- 02basics/myFunction.ts
+++ 02basics/myFunction.ts
@@ -1,6 +1,7 @@
-function addTwo(num: number){
-    return num + 2;
-}
+// function addTwo(num: number){
+//     return num + 2;
+// }
+
 
 
 function getUpper(val:string)
@@ -19,3 +20,33 @@ signUpUser("kalki","email","password",false)
 let loginUser = (name:string,email:string, isPaid:boolean=false) => {}
 loginUser("kalki","email")
 export {}
+function addTwo(num: number):number{
+    return num + 2;
+}
+
+function getValue(myVal:number)
+{
+    if(myVal>10)
+    {
+        return "greater than 10";
+    }
+    return true;
+}
+
+const getHello = (s:string):string =>{
+    return ""
+}
+
+const heros = ["thor","iron man","captain america","hulk"]
+heros.map((hero) :string => {
+    return `${hero} is a hero`
+})
+
+function consoleError(errorMess:string):void
+{
+    console.log(errorMess);
+}
+function HandlerError(errorMess:string):never
+{
+    throw new Error(errorMess);
+}
