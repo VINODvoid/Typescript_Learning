86dcac097b7ad60769d0b60bace5cb07e2f085f9
VINODvoid <iamlearner.v@gmail.com>
2023-12-13 05:40:34 +0530

    data types number boolean  string

---
+++ 01intro/variable.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var greeting = "Hello World";
+greeting.toLowerCase();
+console.log(greeting);
---
+++ 01intro/variable.ts
@@ -0,0 +1,17 @@
+let greeting: string = "Hello World";
+
+greeting.toLowerCase();
+console.log(greeting);
+
+
+// data types
+//? 1 . number
+let userId :number = 3487.12;
+
+//? 2 . boolean
+let isLoggedIn :boolean = true;
+
+//? 3 . string
+let userName :string = "John Doe";
+
+export {}
